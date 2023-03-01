3c59604c9747811de14434272a6217cb63ef19a9
ArchitGandotra <76213767+ArchitGandotra@users.noreply.github.com>
2023-03-01 09:32:15 +0530

    Add files via upload

---
+++ VAC_DSA/Day1.py
@@ -0,0 +1,85 @@
+# #print the prime num range 1 to n........
+# upto=int(input())
+# print("prime numbers ") 
+# if i(i==n)
+
+# collegeName='miet'
+# branch='CSE'
+# age=21
+# print(collegeName,branch,age,sep='\n')
+
+# num=input("Enter the number\n")
+# name=input("Enter the name\n")
+# print(type(name))
+
+# num=3
+# print(type(num))
+
+# ip = list(map(int.input().split(".")))
+# print(ip,end=" ")
+
+# x=list(map(int,input().split( ))) #seperates the values....... and returns list.....
+# print(x)                 
+
+# s=90
+# print(type(s))
+
+# s = [True, 3, 3.2, 'ss', 3+5j]
+# print(type(s))
+
+# s=[234,45,34,67,78]
+# print(type(s))
+
+#operators ====== + - * / // ** %
+# ** power of
+# // modular div
+
+# a = int(input())
+# print(a%2)
+
+#relational operator
+# print(7>0)
+
+#assignment opertor
+# a+=b
+
+#membership operator
+#in, not in
+
+#identity operator
+# is is not
+
+# logical operator
+# and or not 
+
+# a= ( 8 and 4)
+# print(a) 
+
+# marks=int(input())
+# print(marks>40 and marks<80)
+
+# check even odd using bit manipulation 
+
+# num = 10010101
+# if num &2:
+#     print('even')
+# else:
+#     print('10011101')
+
+
+#write a program to toggle k the bit of binary number
+
+#write a program to count total number of unset bit in given number.....
+
+
+#write a program to find the number of set and unset bits
+
+#write a program to find the difference of set and unset bits in number
+
+# variables
+# datatypes
+# operators
+
+#write a program to find largest and smallest number between three number..........
+
+
